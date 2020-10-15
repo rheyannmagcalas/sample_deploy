97da97bde6e19fc7dcc1cfd47c897b3462519740
Rhey Ann <rheyannmagcalas@gmail.com>
2020-10-15 18:55:38 +0800

    Initial Commit

---
+++ sample.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import streamlit as st
+
+from bokeh.plotting import figure, output_file
+from bokeh.models import ColumnDataSource
+from bokeh.palettes import Spectral
+from bokeh.palettes import Spectral6, Magma, Inferno
+from bokeh.themes import built_in_themes
+from bokeh.io import curdoc
+
+from datetime import date, timedelta
+from IPython import get_ipython
+from PIL import Image
+from streamlit import caching
+
+
+# In[16]:
+
+
+st.title('Sample Deploy')
+
